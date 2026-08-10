744a2c736d292527dd293bb470fcae4b06e29c19
ivwvwxwvwvi <67538503+ivwvwxwvwvi@users.noreply.github.com>
2026-08-10 13:52:00 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_LrMdc1wNheY3cgU1jocn9FwrMAFwpLw",
+  authDomain: "childcare-schedule.firebaseapp.com",
+  databaseURL: "https://childcare-schedule-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "childcare-schedule",
+  storageBucket: "childcare-schedule.firebasestorage.app",
+  messagingSenderId: "941657534679",
+  appId: "1:941657534679:web:18c7fadb28320224a94092"
+};
